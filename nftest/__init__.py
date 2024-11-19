e83b20bbf98f89b1e42f9d8c95f1c652d9cfd12c
Nicholas Wiltsie <nwiltsie@mednet.ucla.edu>
2024-11-18 16:44:29 -0800

    Move package definition into pyproject.toml

--- nftest/__init__.py
+++ nftest/__init__.py
@@ -1,3 +1,10 @@
-"""nftest module"""
+"""NFTest module."""
 
-__version__ = "1.1.0"
+try:
+    from nftest._version import version as __version__  # noqa: F401
+except ModuleNotFoundError as err:
+    # The user is probably trying to run this without having installed
+    # the package, so complain.
+    raise RuntimeError(
+        "NFTest is not correctly installed. Please install it with pip."
+    ) from err
